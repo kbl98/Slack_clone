c29ba3ca1ef5c3de9243ba0e7be31a3c01a23d5a
Dr. Katja Becker-Lindhorst <110469926+kbl98@users.noreply.github.com>
2023-03-26 17:06:25 +0200

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'slack-cd3d1',
+    appId: '1:958779556753:web:ceb7a55caee4e497e05030',
+    storageBucket: 'slack-cd3d1.appspot.com',
+    apiKey: 'AIzaSyDKWDlbPuXkdqgp7bK6N0VBub6zoJ-DMgQ',
+    authDomain: 'slack-cd3d1.firebaseapp.com',
+    messagingSenderId: '958779556753',
+  },};
